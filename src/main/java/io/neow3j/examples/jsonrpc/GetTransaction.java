613a0451a7759eadb9a2308aa17e75efc01221ff
Guilherme Sperb Machado <guil@axlabs.com>
2019-03-29 10:15:06 +0100

    Add GetTransaction and GetRawTransaction examples.

---
+++ src/main/java/io/neow3j/examples/jsonrpc/GetTransaction.java
@@ -0,0 +1,18 @@
+package io.neow3j.examples.jsonrpc;
+
+import io.neow3j.protocol.Neow3j;
+import io.neow3j.protocol.core.methods.response.NeoGetTransaction;
+import io.neow3j.protocol.http.HttpService;
+
+import java.io.IOException;
+
+public class GetTransaction {
+
+    public static void main(String[] args) throws IOException {
+        Neow3j neow3j = Neow3j.build(new HttpService("https://seed0.cityofzion.io:443"));
+
+        NeoGetTransaction neoGetTransaction = neow3j.getTransaction("d5a63225e419aca0c10520708c531d36d9912eef0e12cec04e29568930cc4bf9").send();
+        System.out.println("Transaction (verbose=1): " + neoGetTransaction.getTransaction());
+    }
+
+}
