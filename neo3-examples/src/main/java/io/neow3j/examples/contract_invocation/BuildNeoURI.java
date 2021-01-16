45630f4bcb093a4c2a6126228e3927be9b0f4cf3
Michael Bucher <michael@axlabs.com>
2021-01-16 19:40:46 +0100

    Simplified an example to only create a NEP-9 URI

---
+++ neo3-examples/src/main/java/io/neow3j/examples/contract_invocation/BuildNeoURI.java
@@ -0,0 +1,24 @@
+package io.neow3j.examples.contract_invocation;
+
+import io.neow3j.contract.NeoToken;
+import io.neow3j.contract.NeoURI;
+import io.neow3j.contract.ScriptHash;
+
+public class BuildNeoURI {
+
+    public static void main(String[] args) throws Throwable {
+
+        ScriptHash receiver = ScriptHash.fromAddress("NLnyLtep7jwyq1qhNPkwXbJpurC4jUT8ke");
+
+        NeoURI uri = new NeoURI()
+                .amount(22)
+                .asset(NeoToken.SCRIPT_HASH)
+                .toAddress(receiver);
+
+        String uriAsString = uri.buildURI().getURIAsString();
+
+        System.out.println("\n####################");
+        System.out.println("NEP-9 URI: '" + uriAsString + "'");
+        System.out.println("####################\n");
+    }
+}
