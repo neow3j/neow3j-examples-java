d69c4391d54e97934da6c03a9bf4a86e8943d4db
Michael Bucher <michael@axlabs.com>
2021-05-11 12:25:52 +0200

    Update example accounts

--- neo-n3-examples/src/main/java/io/neow3j/examples/Constants.java
+++ neo-n3-examples/src/main/java/io/neow3j/examples/Constants.java
@@ -12,13 +12,17 @@ public class Constants {
     // This sets up the connection to the neo-node of our private network.
     public static Neow3j NEOW3J = Neow3j.build(new HttpService("http://127.0.0.1:40332"));
 
+    public static Neow3j NEOW3J_TESTNET =
+            Neow3j.build(new HttpService("http://seed2t.neo.org:20332"));
+
     // This is Alice' account which is also available on the pre-configured private network.
     public static Account ALICE =
-            Account.fromWIF("L24Qst64zASL2aLEKdJtRLnbnTbqpcRNWkWJ3yhDh2CLUtLdwYK2");
+            Account.fromWIF("L1eV34wPoj9weqhGijdDLtVQzUpWGHszXXpdU9dPuh2nRFFzFa7E");
+//            Account.fromWIF("Kyz7AyXZiEpGT8gUcBGnHRz9GFQ6pXuSeMzuM6wDUioAz6mQsZQx");
 
     // This is Bob's account which is also available on the pre-configured private network.
     public static Account BOB =
-            Account.fromWIF("L3gSLs2CSRYss1zoTmSB9hYAxqimn7Br5yDomH8FDb6NDsupeRVK");
+            Account.fromWIF("L3cNMQUSrvUrHx1MzacwHiUeCWzqK2MLt5fPvJj9mz6L2rzYZpok");
 
     // The genesis account holds all NEO and GAS at the beginning of a new Neo blockchain. The
     // genesis account is a multi-sig account. In our case it is made up of only one account,
