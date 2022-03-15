b006ad3a8df21bea6ee2d9974cbe853a14699c3c
Michael Bucher <michael@axlabs.com>
2022-03-15 11:14:33 +0100

    Simplify SubscribeToContractNotifications

--- src/main/java/io/neow3j/examples/jsonrpc/SubscribeToContractNotifications.java
+++ src/main/java/io/neow3j/examples/jsonrpc/SubscribeToContractNotifications.java
@@ -10,7 +10,6 @@ import io.neow3j.types.Hash256;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static io.neow3j.examples.Constants.NEOW3J;
 
@@ -21,32 +20,27 @@ public class SubscribeToContractNotifications {
         // The contract hash to track.
         Hash160 contractHash = GasToken.SCRIPT_HASH;
 
-        NEOW3J.subscribeToNewBlocksObservable(true)
-                .subscribe((blockReqResult) -> {
-                    NeoBlock block = blockReqResult.getBlock();
-                    System.out.println("#######################################");
-                    System.out.println("Block Index: " + block.getIndex());
+        NEOW3J.subscribeToNewBlocksObservable(true).subscribe((blockReqResult) -> {
+            NeoBlock block = blockReqResult.getBlock();
+            System.out.println("#######################################");
+            System.out.println("Block Index: " + block.getIndex());
 
-                    List<Hash256> transactionsWithNotification = new ArrayList<>();
-                    for (Transaction t : block.getTransactions()) {
-                        List<NeoApplicationLog.Execution> execs =
-                                NEOW3J.getApplicationLog(t.getHash()).send().getApplicationLog().getExecutions();
-                        List<NeoApplicationLog.Execution.Notification> contractNotifications = new ArrayList<>();
-                        for (NeoApplicationLog.Execution e : execs) {
-                            contractNotifications = e.getNotifications().stream()
-                                    .filter(n -> n.getContract().equals(contractHash))
-                                    .collect(Collectors.toList());
-                        }
-                        if (!contractNotifications.isEmpty()) {
-                            transactionsWithNotification.add(t.getHash());
-                        }
-                    }
-                    if (transactionsWithNotification.isEmpty()) {
-                        System.out.println("No contract notification found.");
-                    } else {
-                        System.out.println(
-                                "Contract notification found in transactions: " + transactionsWithNotification);
-                    }
-                });
+            List<Hash256> transactionsWithNotification = new ArrayList<>();
+            for (Transaction t : block.getTransactions()) {
+                List<NeoApplicationLog.Execution> execs =
+                        NEOW3J.getApplicationLog(t.getHash()).send().getApplicationLog().getExecutions();
+
+                boolean notificationFound = execs.stream().anyMatch(
+                        e -> e.getNotifications().stream().anyMatch(n -> n.getContract().equals(contractHash)));
+                if (notificationFound) {
+                    transactionsWithNotification.add(t.getHash());
+                }
+            }
+            if (transactionsWithNotification.isEmpty()) {
+                System.out.println("No contract notification found.");
+            } else {
+                System.out.println("Contract notification found in transactions: " + transactionsWithNotification);
+            }
+        });
     }
 }
