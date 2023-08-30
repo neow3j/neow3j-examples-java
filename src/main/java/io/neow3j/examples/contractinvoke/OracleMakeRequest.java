3362d94f370715a7191689e82c4a68c7b009d65c
Michael Bucher <michael@axlabs.com>
2023-08-30 17:43:51 +0200

    Improve Oracle Example

--- src/main/java/io/neow3j/examples/contractinvoke/OracleMakeRequest.java
+++ src/main/java/io/neow3j/examples/contractinvoke/OracleMakeRequest.java
@@ -2,18 +2,14 @@ package io.neow3j.examples.contractinvoke;
 
 import io.neow3j.contract.SmartContract;
 import io.neow3j.protocol.Neow3j;
+import io.neow3j.protocol.core.response.NeoSendRawTransaction;
 import io.neow3j.transaction.TransactionBuilder;
 import io.neow3j.types.Hash160;
-import io.neow3j.types.Hash256;
 
-import java.math.BigDecimal;
-import java.math.BigInteger;
-
-import static io.neow3j.contract.Token.toFractions;
 import static io.neow3j.examples.Constants.ALICE;
 import static io.neow3j.examples.Constants.NEOW3J_TESTNET;
+import static io.neow3j.examples.Utils.trackSentTransaction;
 import static io.neow3j.transaction.AccountSigner.none;
-import static io.neow3j.types.ContractParameter.integer;
 import static io.neow3j.types.ContractParameter.string;
 
 public class OracleMakeRequest {
@@ -23,23 +19,18 @@ public class OracleMakeRequest {
 
     public static void main(String[] args) throws Throwable {
         // The contract CallOracleContract is deployed on testnet with the following address.
-        Hash160 contractHash = new Hash160("ae2e396f6b082cadeb74d13d978960fa90af28ed");
-        SmartContract smartContract = new SmartContract(contractHash, neow3j);
+        Hash160 contractHash = new Hash160("f1922e6e590acfb52e52fa1c0d689c52ebe9a8e2");
+        SmartContract oracleExampleContract = new SmartContract(contractHash, neow3j);
 
-        BigInteger gasForResponse = toFractions(BigDecimal.ONE, 8);
-        TransactionBuilder b = smartContract.invokeFunction("request",
-                // Change the index 1 to 2 (i.e., 'todos/2') in order to see whether the contract storage was actually
-                // updated.
-                string("https://jsonplaceholder.typicode.com/todos/1"),
-                string(""), // the filter
-                integer(gasForResponse)); // the amount of GAS provided for creating the response tx
-        Hash256 hash = b.signers(none(ALICE))
+        TransactionBuilder b = oracleExampleContract.invokeFunction("request",
+                string("https://jsonplaceholder.typicode.com/users"), // the url
+                string("$[*].email")); // the filter -> here: get the email of all users
+        NeoSendRawTransaction response = b.signers(none(ALICE))
                 .sign()
-                .send()
-                .getSendRawTransaction()
-                .getHash();
+                .send();
 
-        System.out.println("Request transaction hash: " + hash);
+        System.out.println("Request transaction hash: " + response.getSendRawTransaction().getHash());
+        trackSentTransaction(response, neow3j);
     }
 
 }
